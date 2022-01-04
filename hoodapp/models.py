28ff9608e69dcc36c00c1f621213d8ca088738f4
asandelarvine <asandelarvine@gmail.com>
2022-01-05 00:11:45 +0300

    location class set

--- hoodapp/models.py
+++ hoodapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import datetime as dt
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+from django.db.models.fields import TextField
 
 # Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=30)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    def save_location(self):
+        self.save()
+    def __str__(self):
+        return self.name
