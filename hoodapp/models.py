049103926d548b7d1bb0a8eebf0d0f4444261704
asandelarvine <asandelarvine@gmail.com>
2022-01-05 00:14:56 +0300

    added a neighbourhood class and function to update neighborhood

--- hoodapp/models.py
+++ hoodapp/models.py
@@ -14,3 +14,31 @@ class Location(models.Model):
         self.save()
     def __str__(self):
         return self.name
+
+class NeighborHood(models.Model):
+    name = models.CharField(max_length=50)
+    photo = CloudinaryField("image",null=True)
+    content = models.TextField(max_length=600, null=True)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE,null=True)
+    occupants_count = models.IntegerField(default=0)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
+    created_on = models.DateTimeField(auto_now_add=True,null=True)
+    updated_on = models.DateTimeField(auto_now=True,null=True)
+    def create_neigborhood(self):
+        self.save()
+    @classmethod
+    def delete_neighborhood(cls, id):
+        cls.objects.filter(id=id).delete()
+    @classmethod
+    def update_neighborhood(cls, id):
+        cls.objects.filter(id=id).update()
+    @classmethod
+    def search_by_name(cls, search_term):
+        hood = cls.objects.filter(name__icontains=search_term)
+        return hood
+    @classmethod
+    def find_neigborhood(cls, id):
+        hood = cls.objects.get(id=id)
+        return hood
+    def __str__(self):
+        return self.name
