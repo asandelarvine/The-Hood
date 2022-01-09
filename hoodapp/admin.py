85c7329fdc06db5f08e47c14ce613489f84cda43
asandelarvine <asandelarvine@gmail.com>
2022-01-09 16:13:31 +0300

    registered Profile,Neighborhood,Location,Business,Authorities,Post,Category,Health

--- hoodapp/admin.py
+++ hoodapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import  Profile,NeighborHood,Location,Business,Authorities,Post,Category,Health
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(NeighborHood)
+admin.site.register(Location)
+admin.site.register(Business)
+admin.site.register(Authorities)
+admin.site.register(Post)
+admin.site.register(Category)
+admin.site.register(Health)
