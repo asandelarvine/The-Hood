bd239bc54d2a77cf1ddeee4c33fd75b4fa3faec6
asandelarvine <asandelarvine@gmail.com>
2022-01-13 02:12:52 +0300

    clear git cache

--- hoodapp/tests.py
+++ hoodapp/tests.py
@@ -1,13 +1,13 @@
 from django.test import TestCase
-from .models import NeighborHood, NeighbourHood,Business,Post
+from .models import NeighborHood,Business,Post
 from django.contrib.auth.models import User
 # Create your tests here.
 class TestNeighbour(TestCase):
     def setUp(self):
         self.new_hood=NeighborHood(name="Membley",location="Membley",occupationCount=20)
-        self.new_business=Business(businessesName='Shoe it up',user=User(1),neighbor=NeighborHood(1),email='@gmail.com')
-        self.new_user=User(username='collo')
-        self.new_post=Post(image='img.png',post='liked',user=User(1),neighbor=NeighborHood(0))
+        self.new_business=Business(businessesName='Shoe it up',user=User(1),neighborhood=NeighborHood(1),email='@gmail.com')
+        self.new_user=User(username='larvine')
+        self.new_post=Post(image='img.png',post='liked',user=User(1),neighborhood=NeighborHood(0))
     def test_initialization(self):
         self.assertTrue(self.new_hood.name,'Membley')
         self.assertTrue(self.new_hood.location,'Membley')
@@ -16,19 +16,19 @@ class TestNeighbour(TestCase):
     def test_instance(self):
         self.assertTrue(isinstance(self.new_hood,NeighborHood))
 
-    def test_create_neigborhood(self):
-        self.new_hood.create_neigborhood()
+    def test_create_neighborhood(self):
+        self.new_hood.create_neighborhood()
         hood=NeighborHood.objects.all()
         self.assertTrue(len(hood)>0)
 
-    def test_delete_neigborhood(self):
-        self.new_hood.delete_neigborhood()
+    def test_delete_neighborhood(self):
+        self.new_hood.delete_neighborhood()
         hood=NeighborHood.objects.all()
         self.assertEqual(len(hood),0)
 
-    def test_find_neigborhood(self):
-        self.new_hood.create_neigborhood()
-        self.new_hood.find_neigborhood(1)
+    def test_find_neighborhood(self):
+        self.new_hood.create_neighborhood()
+        self.new_hood.find_neighborhood(1)
         hood=NeighborHood.objects.all()
         self.assertEqual(len(hood),1)
 
@@ -43,7 +43,7 @@ class TestNeighbour(TestCase):
 
     def test_create_business(self):
         self.new_user.save()
-        self.new_hood.create_neigborhood()
+        self.new_hood.create_neighborhood()
         self.new_business.create_business()
         busines=Business.objects.all()
         self.assertTrue(len(busines)>0)
@@ -68,15 +68,14 @@ class TestNeighbour(TestCase):
 
 
     def test_correct_initialization(self):
-        self.assertEqual(self.new_feed.image,'img.png')
-        self.assertEqual(self.new_feed.post,'liked')
-        self.assertEqual(self.new_feed.user,User(1))
-        self.assertEqual(self.new_feed.neighbor,NeighborHood(0))
+        self.assertEqual(self.new_post.image,'img.png')
+        self.assertEqual(self.new_post.user,User(1))
+        self.assertEqual(self.new_post.neighborhood,NeighborHood(0))
 
     def test_post_save(self):
         self.new_user.save()
-        self.new_hood.create_neigborhood()
-        self.new_feed.save_post()
+        self.new_hood.create_neighborhood()
+        self.new_post.save_post()
         post=Post.objects.all()
         self.assertTrue(len(post)>0)
 # class TearDown(TestCase):
--- hoodproject/settings.py
+++ hoodproject/settings.py
@@ -32,8 +32,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qm0bvu9gxh=6^^5lr02+1pt#3+w)_4mk#v$tzpy2@(zbk@nw3!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
